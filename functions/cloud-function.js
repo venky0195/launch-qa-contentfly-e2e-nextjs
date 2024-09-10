604522337d712bb9618aedb5c1ff97a69af574c7
Venkatesh G <46915517+venky0195@users.noreply.github.com>
2024-09-10 13:33:35 +0530

    add a log in cf

--- functions/cloud-function.js
+++ functions/cloud-function.js
@@ -1,4 +1,5 @@
 export default function handler(_, res) {
+  console.log("process.env", JSON.stringify(process.env))
   res
     .status(200)
     .json({ deploymentIdentifier: "contentfly-deployment-agent-dev-deploy:7bb9b38cf1f1:run-counter-2" })
